ec28bc96af3183cfae35ff2ec509a46cc27bb719
Timmy Kokke <mental_c@hotmail.com>
2018-11-05 14:40:42 +0100

    Updated agenda, recent works and sw

--- static/sw.js
+++ static/sw.js
@@ -1,23 +1,21 @@
-//This is the "Offline copy of pages" service worker
 
-//Install stage sets up the index page (home page) in the cache and opens a new cache
+var cacheName = 'TimmyKokke-Offline';
+
 self.addEventListener('install', function(event) {
   var indexPage = new Request('index.html');
   event.waitUntil(
     fetch(indexPage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+      return caches.open(cacheName).then(function(cache) {
         return cache.put(indexPage, response);
       });
-  }));
+    })
+  );
 });
 
-//If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
-    return caches.open('pwabuilder-offline').then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
+    return caches.open(cacheName).then(function (cache) {
+      return fetch(request.clone()).then(function (response) {
         return cache.put(request, response);
       });
     });
@@ -27,17 +25,12 @@ self.addEventListener('fetch', function(event) {
 
   event.respondWith(
     fetch(event.request).catch(function(error) {
-      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-
-      //Check to see if you have it in the cache
-      //Return response
-      //If not in the cache, then return error page
-      return caches.open('pwabuilder-offline').then(function (cache) {
-        return cache.match(event.request).then(function (matching) {
+      return caches.open(cacheName).then(function(cache) {
+        return cache.match(event.request).then(function(matching) {
           var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report
+          return report;
         });
       });
     })
   );
-})
+});
