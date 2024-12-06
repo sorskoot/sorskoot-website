d7ee4f42a7b238c7e37371b51b89a884aab76599
Timmy Kokke <mental_c@hotmail.com>
2024-12-06 11:15:48 +0100

    Forgot to commit

--- static/sw.js
+++ static/sw.js
@@ -6,11 +6,11 @@ const files = [];
  * On install, pre-cache provided resources.
  */
 self.addEventListener('install', event => {
-  // Ask the service worker to keep installing until the returning promise
-  // resolves.
-  event.waitUntil(caches.open(CacheName)
-    .then(cache => cache.addAll(files))
-    .then(self.skipWaiting()));
+    // Ask the service worker to keep installing until the returning promise
+    // resolves.
+    event.waitUntil(caches.open(CacheName)
+        .then(cache => cache.addAll(files))
+        .then(self.skipWaiting()));
 });
 
 /** 
@@ -18,7 +18,7 @@ self.addEventListener('install', event => {
 * the next reload for the page.
 */
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+    event.waitUntil(self.clients.claim());
 });
 
 /** 
@@ -26,23 +26,25 @@ self.addEventListener('activate', (event) => {
 * Afterwards, update the cache.
 */
 self.addEventListener('fetch', (evt) => {
-  if (evt.request.method === 'POST') {
-    return;
-  }
+    if (evt.request.method === 'POST') {
+        return;
+    }
 
-  // only handle requests from my domain.
-  if (new URL(evt.request.url).origin === location.origin) {
-    // This promise resolves with either the cached response or a network response
-    evt.respondWith(fromCache(evt.request).catch(() =>
-      //Request is not in the cache, so fetch it from the network 
-      fetch(evt.request.clone())
-    ));
+    const requestUrl = new URL(evt.request.url);
+    // only handle requests from my domain.
+    if (requestUrl.origin === location.origin &&
+        ~requestUrl.origin.match(/localhost/)) {
+        // This promise resolves with either the cached response or a network response
+        evt.respondWith(fromCache(evt.request).catch(() =>
+            //Request is not in the cache, so fetch it from the network 
+            fetch(evt.request.clone())
+        ));
 
-    // Wait until the cache is updated.
-    evt.waitUntil(
-      updateCache(evt.request)
-    );
-  } 
+        // Wait until the cache is updated.
+        evt.waitUntil(
+            updateCache(evt.request)
+        );
+    }
 });
 
 /**
@@ -51,13 +53,13 @@ self.addEventListener('fetch', (evt) => {
 * @returns {Promise<Response>} The response from the cache
 */
 async function fromCache(request) {
-  const cache = await caches.open(CacheName);
-  const matching = await cache.match(request, { ignoreSearch: true });
-  if (!matching) {
-    // throwing the error will cause the promise to reject
-    throw new Error('No match found in cache')
-  };
-  return matching;
+    const cache = await caches.open(CacheName);
+    const matching = await cache.match(request, { ignoreSearch: true });
+    if (!matching) {
+        // throwing the error will cause the promise to reject
+        throw new Error('No match found in cache')
+    };
+    return matching;
 }
 
 /**
@@ -66,14 +68,14 @@ async function fromCache(request) {
 * @returns {Promise<Response>} The response fetched from the network
 */
 async function updateCache(request) {
-  const cache = await caches.open(CacheName);
-  try {
-    const response = await fetch(request);
-    await cache.put(request, response.clone());
-    return response;
-  } catch (e) {
-    // if the fetch fails, just log it and continue.
-    // no need to stall the request
-    console.warn('failed to update cache for ' + request.url);
-  }
+    const cache = await caches.open(CacheName);
+    try {
+        const response = await fetch(request);
+        await cache.put(request, response.clone());
+        return response;
+    } catch (e) {
+        // if the fetch fails, just log it and continue.
+        // no need to stall the request
+        console.warn('failed to update cache for ' + request.url);
+    }
 }
